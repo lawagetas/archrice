f207574f30df7dfe5c279e06f89ea0ba8c8f2f56
lawagetas <l@wanax.xyz>
2025-06-29 14:19:34 -0400

    Default battery profile is power-saver

--- install/power.sh
+++ install/power.sh
@@ -4,7 +4,7 @@ yay -S --noconfirm power-profiles-daemon
 
 if ls /sys/class/power_supply/BAT* &>/dev/null; then
   # This computer runs on a battery
-  powerprofilesctl set balanced
+  powerprofilesctl set power-saver
 else
   # This computer runs on power outlet
   powerprofilesctl set performance
