ea3205880e6d76471dadb89dc6e2c3e81b1f2237
lawagetas <l@wanax.xyz>
2025-06-29 14:22:54 -0400

    removed zoom

--- install/xtras.sh
+++ install/xtras.sh
@@ -1,5 +1,5 @@
 yay -S --noconfirm --needed \
-  signal-desktop spotify dropbox-cli zoom \
+  signal-desktop spotify dropbox-cli \
   obsidian typora libreoffice obs-studio kdenlive \
   pinta xournalpp
 
