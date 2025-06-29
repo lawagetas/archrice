60dde5f9fb76ff8c0676ef310a13783e680d52f6
lawagetas <l@wanax.xyz>
2025-06-29 08:11:34 -0400

    Updated fish config to autostart hyprland

--- install/hyprlandia.sh
+++ install/hyprlandia.sh
@@ -4,4 +4,5 @@ yay -S --noconfirm --needed \
   xdg-desktop-portal-hyprland xdg-desktop-portal-gtk
 
 # Start Hyprland on first session
-echo "[[ -z \$DISPLAY && \$(tty) == /dev/tty1 ]] && exec Hyprland" >~/.bash_profile
+# echo "[[ -z \$DISPLAY && \$(tty) == /dev/tty1 ]] && exec Hyprland" >~/.bash_profile
+echo -e "if status --is-login\n\tif test (tty) = /dev/tty1\n\t\texec Hyprland\n\tend\nend" >~/.config/fish/config.fish
