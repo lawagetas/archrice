1bd02a54d2dac69fc6c94cc70d1eb049c06a1608
lawagetas <l@wanax.xyz>
2025-06-29 15:54:29 -0400

    updated paths from omarchy to archrice

--- install/4-config.sh
+++ install/4-config.sh
@@ -35,7 +35,7 @@ fi
 
 # Set default XCompose that is triggered with CapsLock
 tee ~/.XCompose >/dev/null <<EOF
-include "%H/.local/share/omarchy/default/xcompose"
+include "%H/.local/share/archrice/default/xcompose"
 
 # Identification
 <Multi_key> <space> <n> : "$ARCHRICE_USER_NAME"
