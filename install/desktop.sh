1623061232fc8f9bf24ba7064b6810332d60356f
lawagetas <l@wanax.xyz>
2025-06-29 19:44:56 -0400

    added qt requirements for keepassx; added nautilus plugin for dropbox

--- install/desktop.sh
+++ install/desktop.sh
@@ -2,7 +2,8 @@ yay -S --noconfirm --needed \
   brightnessctl playerctl pamixer pavucontrol wireplumber \
   fcitx5 fcitx5-gtk fcitx5-qt fcitx5-configtool \
   wl-clip-persist clipse \
-  nautilus sushi keepassxc \
+  nautilus nautlus-dropbox sushi keepassxc \
+  qt5-wayland qt6-wayland \
   brave-bin vlc \
   slack-desktop telegram-desktop \
   evince imv \
