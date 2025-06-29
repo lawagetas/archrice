67cf22425657a27588f82ad5751656dacd3545d6
lawagetas <l@wanax.xyz>
2025-06-29 07:30:05 -0400

    Use brave instead of Chromium and keepassxc instead of 1password

--- install/desktop.sh
+++ install/desktop.sh
@@ -2,7 +2,6 @@ yay -S --noconfirm --needed \
   brightnessctl playerctl pamixer pavucontrol wireplumber \
   fcitx5 fcitx5-gtk fcitx5-qt fcitx5-configtool \
   wl-clip-persist clipse \
-  nautilus sushi gnome-calculator \
-  1password-beta 1password-cli \
-  chromium vlc \
+  nautilus sushi keepassxc \
+  brave-bin vlc \
   evince imv
