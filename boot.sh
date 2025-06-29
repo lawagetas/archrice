0100f6af1c8f4ff0eeb214a88def8e27d983df88
lawagetas <l@wanax.xyz>
2025-06-29 14:05:41 -0400

    Install networkmanager before starting installation

--- boot.sh
+++ boot.sh
@@ -16,5 +16,8 @@ echo -e "\nCloning Omarchy..."
 rm -rf ~/.local/share/archrice/
 git clone https://github.com/lawagetas/archrice.git ~/.local/share/archrice >/dev/null
 
+echo -e "\nInstalling network manager (nm-tui and nm-applet)"
+sudoo pacman -Sy --noconfirm network manager
+
 echo -e "\nInstallation starting..."
 source ~/.local/share/archrice/install.sh
