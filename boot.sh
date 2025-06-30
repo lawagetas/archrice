d7528b4f90c7b50753e9644948cbead137d979d6
lawagetas <l@wanax.xyz>
2025-06-30 12:49:11 -0400

    fixed erroneous space in boot.sh

--- boot.sh
+++ boot.sh
@@ -24,7 +24,7 @@ rm -rf ~/.local/share/archrice/
 git clone https://github.com/lawagetas/archrice.git ~/.local/share/archrice >/dev/null
 
 echo -e "\nInstalling network manager (nm-tui and nm-applet)"
-sudo pacman -Sy --noconfirm network manager
+sudo pacman -Sy --noconfirm networkmanager
 
 echo -e "\nInstallation starting..."
 source ~/.local/share/archrice/install.sh
