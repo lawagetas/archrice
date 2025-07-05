af2af23cc9f54468b4853d65d7b1a41e0f0265d8
lawagetas <l@wanax.xyz>
2025-07-05 18:26:05 -0500

    Added nwg-displays to easily manage new monitors added/removed

--- install/9-hyprlandia.sh
+++ install/9-hyprlandia.sh
@@ -1,9 +1,9 @@
 yay -S --noconfirm --needed \
   hyprland hyprshot hyprpicker hyprlock hypridle hyprpolkitagent hyprland-qtutils \
   wofi waybar mako swaybg \
-  xdg-desktop-portal-hyprland xdg-desktop-portal-gtk
+  xdg-desktop-portal-hyprland xdg-desktop-portal-gtk \
+  nwg-displays
 
 # Start Hyprland on first session
 echo "[[ -z \$DISPLAY && \$(tty) == /dev/tty1 ]] && exec Hyprland" >~/.bash_profile
 echo "[[ -z \$DISPLAY && \$(tty) == /dev/tty1 ]] && exec Hyprland" >>~/.zprofile
-
