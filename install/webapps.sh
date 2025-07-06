775d9c11d2a5bce23d007f490ed1e7b523f02f65
lawagetas <l@wanax.xyz>
2025-07-05 20:15:08 -0500

    webapps broken. removing for now

--- install/webapps.sh
+++ install/webapps.sh
@@ -1,4 +1,4 @@
-source ~/.local/share/archrice/default/bash/functions
+# source ~/.local/share/archrice/default/bash/functions
 # web2app "WhatsApp" https://web.whatsapp.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/whatsapp.png
 # web2app "Google Photos" https://photos.google.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/google-photos.png
 # web2app "Google Contacts" https://contacts.google.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/google-contacts.png
@@ -6,5 +6,5 @@ source ~/.local/share/archrice/default/bash/functions
 # web2app "ChatGPT" https://chatgpt.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/chatgpt.png
 # web2app "YouTube" https://youtube.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/youtube.png
 # web2app "GitHub" https://github.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/github-light.png
-web2app "X" https://x.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/x-light.png
-web2app "Grok" https://grok.com/ https://registry.npmmirror.com/@lobehub/icons-static-png/1.51.0/files/dark/grok.png
+# web2app "X" https://x.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/x-light.png
+# web2app "Grok" https://grok.com/ https://registry.npmmirror.com/@lobehub/icons-static-png/1.51.0/files/dark/grok.png
