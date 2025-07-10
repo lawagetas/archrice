2bd2bf6f43a79a455efc66e8acb92de218e4629b
lawagetas <l@wanax.xyz>
2025-07-10 12:41:02 -0500

    Update files reviewed and fixed for archrice fork

--- migrations/1751669258.sh
+++ migrations/1751669258.sh
@@ -1,3 +1,5 @@
-echo "Add missing docker config"
-sudo mkdir -p /etc/docker
-echo '{"log-driver":"json-file","log-opts":{"max-size":"10m","max-file":"5"}}' | sudo tee /etc/docker/daemon.json
+## Commented everything out -- not using docker
+
+# echo "Add missing docker config"
+# sudo mkdir -p /etc/docker
+# echo '{"log-driver":"json-file","log-opts":{"max-size":"10m","max-file":"5"}}' | sudo tee /etc/docker/daemon.json
