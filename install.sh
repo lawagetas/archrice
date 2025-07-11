543aa85c1f724790caa0275ed12f43554fe0aaee
lawagetas <l@wanax.xyz>
2025-07-11 16:22:04 -0500

    don't quit if file exists with non zero return

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Exit immediately if a command exits with a non-zero status
-set -e
+# set -e
 
 # Give people a chance to retry running the installation
 trap 'echo "Archrice installation failed! You can retry by running: source ~/.local/share/archrice/install.sh"' ERR
 
 # Install everything
-for f in ~/.local/share/omarchy/install/*.sh; do
+for f in ~/.local/share/archrice/install/*.sh; do
   gum log --time rfc822 --level debug "\nRunning installer: $f"
   source "$f"
 done
