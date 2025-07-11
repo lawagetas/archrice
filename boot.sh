b81a63ae3610cbb81e1083b46e305fc3391604d0
lawagetas <l@wanax.xyz>
2025-07-11 18:27:49 -0500

    install gum

--- boot.sh
+++ boot.sh
@@ -19,6 +19,8 @@ echo -e "\n$ascii_art\n"
 
 pacman -Q git &>/dev/null || sudo pacman -Sy --noconfirm --needed git
 
+pacman -Sy --noconfirm gum
+
 echo -e "\nCloning ArchRice..."
 rm -rf ~/.local/share/archrice/
 git clone https://github.com/lawagetas/archrice.git ~/.local/share/archrice >/dev/null
