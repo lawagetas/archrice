ac9072fb714034d04a82cdc9c0140841ad9b2012
lawagetas <l@wanax.xyz>
2025-07-13 20:23:43 -0500

    Use nm-applet to manage networks

--- install/desktop.sh
+++ install/desktop.sh
@@ -1,5 +1,5 @@
 yay -S --noconfirm --needed \
-  brightnessctl playerctl pamixer pavucontrol wireplumber \
+  brightnessctl playerctl pamixer pavucontrol wireplumber nm-applet \
   fcitx5 fcitx5-gtk fcitx5-qt fcitx5-configtool \
   wl-clip-persist clipse \
   nautilus nautlus-dropbox ffmpegthumbnailer sushi keepassxc \
