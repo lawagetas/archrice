07e7c9bc6362f9bf279926cb115093d2f079ab25
lawagetas <105167885+lawagetas@users.noreply.github.com>
2025-07-16 11:38:19 -0500

    removed nm applet and switched to iwd

--- boot.sh
+++ boot.sh
@@ -25,8 +25,5 @@ echo -e "\nCloning ArchRice..."
 rm -rf ~/.local/share/archrice/
 git clone https://github.com/lawagetas/archrice.git ~/.local/share/archrice >/dev/null
 
-echo -e "\nInstalling network manager (nm-tui and nm-applet)"
-sudo pacman -Sy --noconfirm networkmanager
-
 echo -e "\nInstallation starting..."
 source ~/.local/share/archrice/install.sh
--- install/desktop.sh
+++ install/desktop.sh
@@ -1,5 +1,5 @@
 yay -S --noconfirm --needed \
-  brightnessctl playerctl pamixer pavucontrol wireplumber nm-applet \
+  brightnessctl playerctl pamixer pavucontrol wireplumber \
   fcitx5 fcitx5-gtk fcitx5-qt fcitx5-configtool \
   wl-clip-persist clipse \
   nautilus nautilus-dropbox ffmpegthumbnailer sushi \
